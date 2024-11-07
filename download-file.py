dadccb87bea175cf98c6799c92c9694b0484f409
root <root@root.com>
2024-11-07 09:40:12 +0200

    update-printings (#1)
    Co-authored-by: David Yair <david.yair@mail.com>
    Reviewed-on: http://localhost:3000/root/File-Downloader/pulls/1

--- download-file.py
+++ download-file.py
@@ -6,8 +6,9 @@ import sys
 
 # Configuration variables
 SLEEP_BETWEEN_DOWNLOAD_TRIES_SECONDS = int(os.getenv("SLEEP_BETWEEN_DOWNLOAD_TRIES_SECONDS", "10"))  # Default: 10 seconds
-CHECK_INTERNET_CONNECTION_TIMEOUT    = int(os.getenv("SLEEP_BETWEEN_DOWNLOAD_TRIES_SECONDS", "9"))  # Default: 9 seconds
-SRC_FILE_TO_DOWNLOAD_URL             = os.getenv("SRC_FILE_TO_DOWNLOAD_URL", "https://raw.githubusercontent.com/Davis8988/birthday-dashboard/main/data/birthdays.csv")
+REQUESTS_TIMEOUT_SEC                 = int(os.getenv("REQUESTS_TIMEOUT_SEC", "9"))  # Default: 9 seconds
+READ_CONTENT_TIMEOUT_SEC             = int(os.getenv("READ_CONTENT_TIMEOUT_SEC", "30"))  # Default: 30 seconds
+SRC_FILE_TO_DOWNLOAD_URL             = os.getenv("SRC_FILE_TO_DOWNLOAD_URL", "https://drive.google.com/uc?id=1NMct4riX9-Ip689GzVPl3ILm3ylABoX4")  # Link to: https://drive.google.com/file/d/1NMct4riX9-Ip689GzVPl3ILm3ylABoX4/view?usp=drive_link
 DEST_DOWNLOAD_DIR_PATH               = os.getenv("DEST_DOWNLOAD_DIR_PATH", "/data")
 DEST_DOWNLOAD_FILE_NAME              = os.getenv("DEST_DOWNLOAD_FILE_NAME", "birthdays.csv")
 LOG_LEVEL                            = getattr(logging, os.getenv("LOG_LEVEL", "INFO").upper())
@@ -15,26 +16,26 @@ LOG_LEVEL                            = getattr(logging, os.getenv("LOG_LEVEL", "
 # Set up logging
 logging.basicConfig(stream=sys.stdout, level=LOG_LEVEL, format='%(asctime)s - %(levelname)s: %(message)s')
 
-def check_internet_connection():
-    try:
-        logging.info(f"Checking if URL adderss is available: {SRC_FILE_TO_DOWNLOAD_URL}")
-        logging.info(f"set timeout to: {CHECK_INTERNET_CONNECTION_TIMEOUT} sec")
-        response = requests.head(SRC_FILE_TO_DOWNLOAD_URL, timeout=CHECK_INTERNET_CONNECTION_TIMEOUT)
-        if response.status_code == 200:
-            logging.info("OK - Address is available.")
-            return True
-    except requests.ConnectionError as err_msg:
-        logging.error(err_msg)
-    logging.warning("No internet connection available.")
-    return False
+# def check_internet_connection():
+#     try:
+#         logging.info(f"Checking if URL adderss is available: {SRC_FILE_TO_DOWNLOAD_URL}")
+#         logging.info(f"set timeout to: {REQUESTS_TIMEOUT_SEC} sec")
+#         response = requests.head(SRC_FILE_TO_DOWNLOAD_URL, timeout=REQUESTS_TIMEOUT_SEC)
+#         if response.status_code == 200:
+#             logging.info("OK - Address is available.")
+#             return True
+#     except requests.ConnectionError as err_msg:
+#         logging.error(err_msg)
+#     logging.warning("No internet connection available.")
+#     return False
 
 def download_file(url, local_path):
     logging.info(f"Downloading file: '{url}' to: {local_path}")
     try:
-        logging.info(f"set timeout to: {CHECK_INTERNET_CONNECTION_TIMEOUT} sec")
+        logging.info(f"set timeout to: {REQUESTS_TIMEOUT_SEC} sec")
         logging.info("Downloading now...")
         with open(local_path, 'wb') as f:
-            response = requests.get(url, timeout=CHECK_INTERNET_CONNECTION_TIMEOUT)
+            response = requests.get(url, timeout=(REQUESTS_TIMEOUT_SEC, READ_CONTENT_TIMEOUT_SEC), allow_redirects=True)
             f.write(response.content)
         logging.info("OK - Download complete.")
     except Exception as e:
@@ -46,7 +47,7 @@ def print_vars():
     logging.info("")
     logging.info("Configuration Variables:")
     logging.info(f"  SLEEP_BETWEEN_DOWNLOAD_TRIES_SECONDS : {SLEEP_BETWEEN_DOWNLOAD_TRIES_SECONDS}")
-    logging.info(f"  CHECK_INTERNET_CONNECTION_TIMEOUT    : {CHECK_INTERNET_CONNECTION_TIMEOUT}")
+    logging.info(f"  REQUESTS_TIMEOUT_SEC                 : {REQUESTS_TIMEOUT_SEC}")
     logging.info(f"  SRC_FILE_TO_DOWNLOAD_URL             : {SRC_FILE_TO_DOWNLOAD_URL}")
     logging.info(f"  DEST_DOWNLOAD_DIR_PATH               : {DEST_DOWNLOAD_DIR_PATH}")
     logging.info(f"  DEST_DOWNLOAD_FILE_NAME              : {DEST_DOWNLOAD_FILE_NAME}")
@@ -91,14 +92,17 @@ if __name__ == "__main__":
             logging.info(" ==> Attempt %d", attempt_count)
             logging.info("")
             print_dest_dir_contents()  # <-- Print contents of destination directory before next attempt
-            print_dest_file_content()  # Print content of file "{DEST_DOWNLOAD_FILE_NAME}" before next attempt
-            if not check_internet_connection():
-                logging.warning("Attempt %d: No internet connection available. Skipping download attempt.", attempt_count)
-            else:
-                logging.info("OK - Internet connection available. Proceeding with download...")
+            # if not check_internet_connection():
+            #     logging.warning("Attempt %d: No internet connection available. Skipping download attempt.", attempt_count)
+            # else:
+            #     logging.info("OK - Internet connection available. Proceeding with download...")
 
-                logging.info("Downloading file from URL: %s", SRC_FILE_TO_DOWNLOAD_URL)
-                download_file(SRC_FILE_TO_DOWNLOAD_URL, os.path.join(DEST_DOWNLOAD_DIR_PATH, DEST_DOWNLOAD_FILE_NAME))
+            logging.info("Downloading file from URL: %s", SRC_FILE_TO_DOWNLOAD_URL)
+            download_file(SRC_FILE_TO_DOWNLOAD_URL, os.path.join(DEST_DOWNLOAD_DIR_PATH, DEST_DOWNLOAD_FILE_NAME))
+            logging.info("")
+            print_dest_file_content()  # Print content of file "{DEST_DOWNLOAD_FILE_NAME}" before next attempt
+            logging.info("")
+            print_dest_dir_contents()
                 
             logging.info("Waiting for %d seconds before next download attempt...", SLEEP_BETWEEN_DOWNLOAD_TRIES_SECONDS)
             time.sleep(SLEEP_BETWEEN_DOWNLOAD_TRIES_SECONDS)
